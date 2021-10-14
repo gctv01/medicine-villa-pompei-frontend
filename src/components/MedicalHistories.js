9dcb0cee0973a8821f5deb5c9f5e3ff3137df134
franketa <francohbalik@gmail.com>
2021-10-14 12:39:39 -0300

    savefrank

--- src/components/MedicalHistories.js
+++ src/components/MedicalHistories.js
@@ -1,71 +1,85 @@
-import React, {Component} from "react";
-import axios from 'axios';
-import { Table, Button, Container, Modal, ModalBody, ModalHeader, ModalFooter, FormGroup } from "reactstrap";
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  FormGroup,
+} from "reactstrap";
 
 class MedicalHistories extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      posts: [],
+    };
+  }
 
-        this.state = {
-            posts: []   
-        }
-    }
+  componentDidMount() {
+    const fetchPacientes = async () => {
+      try {
+        await axios.get("http://localhost:3006/pacientes").then((response) => {
+          this.setState({
+            posts: response.data,
+          });
+          console.log(response.data);
+        });
+      } catch (error) {}
+    };
+    fetchPacientes()
+  }
 
-componentDidMount(){
-    axios.get('https://my-json-server.typicode.com/franketa/db.json/pacientes')
-    .then(response =>{
-        this.setState({
-            
-            posts: response.data
-        })
-        console.log(response.data)
-    })
+  render() {
+    const { posts } = this.state;
+    return (
+      <>
+        <Container>
+          <h1>List of posts</h1>
+          <Table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>Nombre</th>
+                <th>Medico</th>
+                <th>Sintomatoligia</th>
+                <th>Intervenciones Quirurgicas</th>
+                <th>Tratamientos Actuales</th>
+                <th>Vacunas</th>
+                <th>Alergias</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map((post) => (
+                <tr>
+                  <td>{post.id}</td>
+                  <td>{post.nombre_completo}</td>
+                  <td>{post.medico}</td>
+                  <td>{post.sintomatologia}</td>
+                  <td>{post.intervenciones_quirurgicas}</td>
+                  <td>{post.tratamientos_actuales}</td>
+                  <td>{post.vacunas}</td>
+                  <td>{post.alergias}</td>
+                  <td>
+                    <Button color="success">Editar</Button>
+                  </td>
+                  <td>
+                    <Button color="success">Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+      </>
+    );
+  }
 }
 
-    
-
-    render(){
-        const {posts} = this.state
-        return(
-
-            <>
-            <Container>
-                <h1>List of posts</h1>
-                <Table>
-                    <thead><tr><th>id</th>
-                    <th>Nombre</th>
-                    <th>Medico</th>
-                    <th>Sintomatoligia</th>
-                    <th>Intervenciones Quirurgicas</th>
-                    <th>Tratamientos Actuales</th>
-                    <th>Vacunas</th>
-                    <th>Alergias</th>
-                    <th>Informacion Adicional</th></tr>
-                    </thead>
-                    <tbody>
-                    {posts.map((post) => 
-                    <tr>
-                        <td>{post.id}</td>
-                        <td>{post.nombre_completo}</td>
-                        <td>{post.medico}</td>
-                        <td>{post.sintomatologia}</td>
-                        <td>{post.intervenciones_quirurgicas}</td>
-                        <td>{post.tratamientos_actuales}</td>
-                        <td>{post.vacunas}</td>
-                        <td>{post.alergias}</td>
-                        <td>{post.informacion_adicional}</td>
-
-                    </tr>)}
-                
-                    </tbody>    
-                </Table>
-                
-            </Container>
-            </>
-
-        )
-        }
-}
-
-export default MedicalHistories
+export default MedicalHistories;
